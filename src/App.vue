82765d6e3426f2d9758ae7ea6c62ec0c6af1d39d
Denis Oleshchenko <denisoleshenko@gmail.com>
2018-08-02 19:43:26 +0300

    add a chart

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   	<div id="app">
+		<h1 class="headline text-md-center font-weight-light font-italic">Statistics of Cryptocurrencies</h1>
 		<v-layout row class="currency-list">
 			<template>
 				<v-expansion-panel>
-					<v-expansion-panel-content v-for="(value, key) in cryptos" class="purple white--text title font-weight-light">
-					<div slot="header">
-						<span class="left">{{ key }}</span>
-						<span class="right">${{ value.USD }}</span>
-					</div>
-					<v-card class="body-1 white black--text">
-						<v-card-text>Here a chart will be placed.</v-card-text>
-					</v-card>
+					<v-expansion-panel-content v-for="(value, key) in cryptos" :key="key"
+						class="purple white--text title font-weight-light">
+						
+						<div slot="header">
+							<span class="left">{{ key }}</span>
+							<span class="right">${{ value.USD }}</span>
+						</div>
+						<v-card class="body-1 white black--text">
+							<!--We are using the LineChart component imported below in the script and 
+								also setting the chart-data prop to the datacollection object-->
+							<line-chart :chart-data="datacollection"></line-chart>
+							<v-card-text>Here a chart will be placed.</v-card-text>
+						</v-card>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 			</template>
 		</v-layout>
+		<line-chart :chart-data="datacollection"></line-chart>
 		<v-footer class="body-2 caption footer" fixed>
 			<p class="text-md-center wide">Powered by ActiveWizards &copy; {{ new Date().getFullYear() }}</p>
 		</v-footer>
@@ -23,35 +30,84 @@
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
+import LineChart from './components/LineChart'
 
 
 export default {
 	name: 'app',
+	components: { LineChart },
 	data: () => ({
 		cryptos: [],
-		errors: []
+		errors: [],
+		historicalData: [],
+		datacollection: null,
+		open: null,
+		close: null
 	}),
+	
   	created () {
+		
+      	//this.fillData()
 		
 		axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT,XRP,EOS,AION,TRX&tsyms=USD')
 			.then(response => {
 				this.cryptos = response.data
-				//console.log(response)         // This will give you access to the full object
 			})
 			.catch(e => {
 				this.errors.push(e)
 			});
-		// for btc/usd
-		const endpoint = 'https://min-api.cryptocompare.com/data/histominute?fsym=' + 'BTC' + '&tsym=' + 'USD' + '&limit=60&aggregate=3&e=CCCAGG';
-		axios.get(endpoint)
-			.then(response => {
-				//this.cryptos = response.data
-				console.log(response)
-			})
-			.catch(e => {
-				this.errors.push(e)
-			});
-  	}
+		this.fetchData()
+	},
+    mounted () {
+    	this.fillData()
+	},
+	methods: {
+		fillData () {
+			console.log('here')
+			// for btc/usd
+			const endpoint = 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=100&aggregate=3&e=CCCAGG';
+			axios.get(endpoint)
+				.then(response => {
+					this.historicalData = response.data.Data;
+					
+					let timeResult = this.historicalData.map(a => a.time); //moment().format('L')
+					let openPosition = this.historicalData.map(a => a.open);
+					let closePosition = this.historicalData.map(a => a.close);
+
+					this.time = timeResult;
+					this.open = openPosition;
+					this.close = closePosition;
+					
+					this.datacollection = {
+						labels: this.time,
+						datasets: [
+							{
+								label: 'Open position',
+								backgroundColor: '#f87979',
+								data: this.open
+							},
+							{
+								label: 'Close position',
+								backgroundColor: '#5bf8bf',
+								data: this.close
+							}
+						]
+					}
+				})
+				.catch(e => {
+					this.errors.push(e)
+				});
+		},
+		
+		addExpenses () {
+		
+		},
+		
+		fetchData () {
+		
+		}
+    }
 }
 
 	
@@ -66,6 +122,10 @@ body, #app {
 
 #app {
 	padding-top: 30px;
+}
+
+h1 {
+	margin-bottom: 10px;
 }
 
 .currency-list {
